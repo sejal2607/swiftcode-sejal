90dc57dc67115e26c9cb51d8a0b7a64c3e67f2b2
sejal2607 <26sejal@gmail.com>
2018-04-14 07:22:15 +0000

    feedreasponseCommit

--- app/services/FeedService.java
+++ app/services/FeedService.java
@@ -3,6 +3,7 @@ package services;
 import data.FeedResponse;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
+import play.libs.XML;
 import play.libs.ws.WS;
 import play.libs.ws.WSRequest;
 import play.libs.ws.WSResponse;
@@ -11,5 +12,22 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
 public class FeedService {
-
+    public FeedResponse getFeedByQuery(String query) {
+        FeedResponse feedResponseObject = new FeedResponse();
+        try {
+            WSRequest feedRequest = WS.url("https://news.google.com/news");
+            CompletionStage<WSResponse> responsePromise = feedRequest
+                    .setQueryParameter("q", query)
+                    .setQueryParameter("output", "rss")
+                    .get();
+            Document feedResponse = responsePromise.thenApply(WSResponse::asXml).toCompletableFuture().get();
+            Node item = feedResponse.getFirstChild().getFirstChild().getChildNodes().item(10);
+            feedResponseObject.title=item.getChildNodes().item(0).getFirstChild().getNodeValue();
+            feedResponseObject.pubDate=item.getChildNodes().item(3).getFirstChild().getNodeValue();
+            feedResponseObject.Description=item.getChildNodes().item(4).getFirstChild().getNodeValue();
+         }
+         catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
